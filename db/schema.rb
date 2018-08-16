7924507c80a3a9cedfb2829238f796be70d138c4
github username <github email address>
2018-08-16 09:21:26 -0400

    rename type

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180816124913) do
+ActiveRecord::Schema.define(version: 20180816132016) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,7 +20,7 @@ ActiveRecord::Schema.define(version: 20180816124913) do
     t.integer "category_price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "type"
+    t.string "type_of"
   end
 
   create_table "items", force: :cascade do |t|
